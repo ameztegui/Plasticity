0322180f2b9bc0fc250e9fbe466dde337f41c2d5
Aitor Ameztegui <ameztegui@gmail.com>
2020-03-18 17:20:54 +0100

    Added function compute_rdpi

--- R/RDPI.R
+++ R/RDPI.R
@@ -1,11 +1,10 @@
-#' @title RDPI
-
+#' @title rdpi
+#' 
 #' @description Function to compute the RDPI (Relative Distance Plasticity Index,
 #' Valladares et al, (2006) Quantitative estimation of phenotypic plasticity:
 #'  bridging the gap between the evolutionary concept and its ecological applications,
 #'  Journal of Ecology, 94(6):1103-1116.
 #'
-
 #' @param dataframe The dataframe that contains the data
 #' @param sp The bare (unquoted) name of the column whose values will be used as independent variable.
 #'The function will compare RDPI values among values of this variable. It can be species, provenances, etc.
@@ -36,16 +35,10 @@ rdpi <- function(dataframe, sp, trait, factor, verbose = T) {
     library(ggplot2)
     library(sciplot)
     
-    # Modify the parameters to add quotes (")
-    # sp <- deparse(substitute(sp))
-    # trait <- deparse(substitute(trait))
-    # factor <- deparse(substitute(factor))
-    
     # Create the object (an empty data frame) that will store the results of RDPI
     RDPI <- data.frame(sp = character(0), value = numeric(0))
     
     # Since we need to compute everything per species, we make a 'for' loop
-    
     levels_dataframe <- levels(dataframe %>% pull({{sp}}) %>% droplevels())
     
     for (a in levels_dataframe) {
@@ -53,39 +46,13 @@ rdpi <- function(dataframe, sp, trait, factor, verbose = T) {
         # subset the data for a given species
         data_sp <- dataframe %>% filter({{sp}} == a)
         
-        # NOTE: RDPI is based on pairwise distances (Canberra distance) between individuals that
-        # belong to different levels of an environmental variable. We perform this in three steps:
-        
-        # Step1: Compute pairwise Canberra distance (aka RDPI) for all individuals in the dataset
-        RDPI_temp <- as.matrix(dist(x = data_sp %>% pull({{trait}}), method="canberra"))
-        
-        # Step 2: Generate a matrix where value is "TRUE" only if observation i and observation j
-        # belong to different levels of the factor
-        filter_frame <- data.frame(matrix(NA, nrow(data_sp), nrow(data_sp)))
-        
-        for (i in 1:nrow(filter_frame)) {
-            for (j in 1:ncol(filter_frame)) {
-                
-                ifelse(pull(data_sp, {{factor}})[i] == pull(data_sp, {{factor}})[j],
-                       filter_frame[i,j] <- FALSE,
-                       filter_frame[i,j] <- TRUE)
-            }
-        }
-        
-        
-        
-        filter_frame[upper.tri(filter_frame, diag = T)] <- FALSE         #only keep lower triangle
-        
-        # Step 3: Subset RDPI so that it only includes comparisons between individuals that
-        # belong to different levels of an environmental variable
-        RDPI_temp <- RDPI_temp[filter_frame == TRUE]
-        
-        
-        RDPI_sp <- data.frame(sp = a,
+        RDPI_temp <- compute_rdpi(data_sp, {{trait}}, {{factor}})
+    
+        RDPI_sp <- data.frame(sp = as.character(a),
                               value = RDPI_temp)
         
         RDPI <- rbind(RDPI, RDPI_sp)
-    }
+     }
     
     # Once everything calculated, let's produce nice outputs
     
@@ -116,7 +83,7 @@ rdpi <- function(dataframe, sp, trait, factor, verbose = T) {
         print("ANOVA test")
         print(summary(fit))
         Tuk <- HSD.test (fit, trt='RDPI$sp')
-        Tuk$groups$sp <- row.names(Tuk$groups)
+        Tuk$groups$sp <- as.factor(row.names(Tuk$groups))
         summary <- left_join(summary, Tuk$groups) %>%
             select(-`RDPI$value`)
         print("Tukey HSD test for differences accross groups")
